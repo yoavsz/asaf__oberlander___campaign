7b996941342e4fef75ca1abe4ad91f5b5a9e713c
yoavsz <139166316+yoavsz@users.noreply.github.com>
2023-09-27 11:55:10 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const calendarDays = document.querySelector(".calendar-days");
   const currentMonthText = document.getElementById("currentMonth");
   const eventsContainer = document.getElementById("events");
-  const mark__list=[26,9,"חוג בית משפחת סדקוביץ",1,10,"לייב פייסבוק בנושא ביטחון",15,10,"שאלות בווטסאפ"
+  const mark__list=[1,10,"לייב פייסבוק בנושא ביטחון",15,10,"שאלות בווטסאפ"
   ,12,10,"חוג בית משפחת הרשקוביץ למתאמני A+",31,10,"יום הבחירות"]
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
